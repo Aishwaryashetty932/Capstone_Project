d31e79ae23b4440b7c28031701e62c87c5f673d6
unknown <aishwaryashetty932@gmail.com>
2025-02-12 09:48:16 +0530

    commit - ITC Capstone Project

--- src/main/java/base/BaseClass.java
+++ src/main/java/base/BaseClass.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 
 public class BaseClass {
