d00eb1645141e443dcf4bb80229b77aae8cac89b
unknown <aishwaryashetty932@gmail.com>
2025-02-17 20:09:23 +0530

    change

--- src/test/java/testcases/PurchaseLaptopTest.java
+++ src/test/java/testcases/PurchaseLaptopTest.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 public class PurchaseLaptopTest extends BaseClass {
 
     @BeforeClass
-    @Parameters("browser")//parameter browser
+    @Parameters("browser")//parameter browser test
     public void setup(String browser) throws IOException {
         openBrowser(browser);  
         driver.get(prop.getProperty("url"));  
